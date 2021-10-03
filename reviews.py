ed2605de052154fe11190a654c043a6078d7ace2
James Kraus <jameskraus@Jamess-Air.attlocal.net>
2021-10-03 19:45:30 -0400

    AAdd and edit the descriptive comments for each part of this module

--- reviews.py
+++ reviews.py
@@ -1,4 +1,4 @@
-#this project displays five star google reviews for James Kraus while employed at Scheller's Fitness and Cycling.
+# This project displays customer reviews for James Kraus while employed at Scheller's Fitness and Cycling.
 
 # Import required packages
 import time
@@ -6,19 +6,21 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-# Add options on chrome
+# Add options on chrome. These options allow selenium to do its work without opening the 
+# browser window, thus speeding up the module.
 chromeOptions = Options()
 chromeOptions.add_argument("--headless")
 chromeOptions.add_argument("--disable-gpu")
 
 
-# locate elements by name
+# Locate elements by name
 browser = webdriver.Chrome(options=chromeOptions)
 browser.get("https://reviews.listen360.com/scheller-s-fitness-cycling-middletown-louisville")
 time.sleep(1)
 
 element = browser.find_element_by_tag_name("body")
 
+# Scroll down on page to load older reviews on the infinite feed.
 no_of_pagedowns = 10
 
 while no_of_pagedowns:
@@ -26,27 +28,8 @@ while no_of_pagedowns:
     time.sleep(0.2)
     no_of_pagedowns-=1
 
+# Find and print only the reviews that contain the name "James"
 review_elements = browser.find_elements_by_xpath("//*[contains(text(), 'James')]")
-
-# review_elements = browser.find_elements_by_class_name("listen360-feedback-summary-span")
 
 for review in review_elements:
     print(review.text)
-
-# page_source_overview = browser.page_source
-
-# from bs4 import BeautifulSoup
-
-# # Load in page source info
-# soup = BeautifulSoup(page_source_overview, 'lxml')
-
-# # text_to_search = soup.find_all('span', class_='listen360-feedback-summary-span')
-
-# reviews = soup.find_all("span", string="James")
-
-# print(len(reviews))
-
-# for review in reviews:
-#     print(reviews)
-
-# browser.quit()
