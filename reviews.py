6f7e2d38ae081b6d06cba20bd8a865f50853c3d2
James Kraus <jameskraus@Jamess-Air.attlocal.net>
2021-10-03 19:39:38 -0400

    found successful method for finding element by text and filtering by keyword using selenium instead of beautiful soup.

--- reviews.py
+++ reviews.py
@@ -8,8 +8,8 @@ from selenium.webdriver.chrome.options import Options
 
 # Add options on chrome
 chromeOptions = Options()
-# chromeOptions.add_argument("--headless")
-# chromeOptions.add_argument("--disable-gpu")
+chromeOptions.add_argument("--headless")
+chromeOptions.add_argument("--disable-gpu")
 
 
 # locate elements by name
@@ -26,9 +26,27 @@ while no_of_pagedowns:
     time.sleep(0.2)
     no_of_pagedowns-=1
 
-# post_elements = browser.find_elements_by_class_name("listen360-feedback-summary-span")
+review_elements = browser.find_elements_by_xpath("//*[contains(text(), 'James')]")
 
-# for post in post_elements:
-#     print(post.text)
+# review_elements = browser.find_elements_by_class_name("listen360-feedback-summary-span")
 
-page_source_overview = browser.page_source
+for review in review_elements:
+    print(review.text)
+
+# page_source_overview = browser.page_source
+
+# from bs4 import BeautifulSoup
+
+# # Load in page source info
+# soup = BeautifulSoup(page_source_overview, 'lxml')
+
+# # text_to_search = soup.find_all('span', class_='listen360-feedback-summary-span')
+
+# reviews = soup.find_all("span", string="James")
+
+# print(len(reviews))
+
+# for review in reviews:
+#     print(reviews)
+
+# browser.quit()
