96c5c112bfc91864b6a12a638a76c99e4c55f843
James Kraus <jameskraus@Jamess-Air.attlocal.net>
2021-10-03 15:47:54 -0400

    add options for headless and disable gpu in selenium to speed up search, begin adding beautifulsoup code

--- reviews.py
+++ reviews.py
@@ -1,25 +1,34 @@
 #this project displays five star google reviews for James Kraus while employed at Scheller's Fitness and Cycling.
 
+# Import required packages
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-browser = webdriver.Chrome()
+# Add options on chrome
+chromeOptions = Options()
+# chromeOptions.add_argument("--headless")
+# chromeOptions.add_argument("--disable-gpu")
 
+
+# locate elements by name
+browser = webdriver.Chrome(options=chromeOptions)
 browser.get("https://reviews.listen360.com/scheller-s-fitness-cycling-middletown-louisville")
 time.sleep(1)
 
-elem = browser.find_element_by_tag_name("body")
+element = browser.find_element_by_tag_name("body")
 
-no_of_pagedowns = 20
+no_of_pagedowns = 10
 
 while no_of_pagedowns:
-    elem.send_keys(Keys.PAGE_DOWN)
+    element.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.2)
     no_of_pagedowns-=1
 
-post_elems = browser.find_elements_by_class_name("listen360-feedback-summary-span")
+# post_elements = browser.find_elements_by_class_name("listen360-feedback-summary-span")
 
-for post in post_elems:
-    print(post.text)
+# for post in post_elements:
+#     print(post.text)
+
+page_source_overview = browser.page_source
