a30ad65bbfa1337cc06caa6ad91542b210788e94
James Kraus <jameskraus@Jamess-Air.attlocal.net>
2021-10-05 17:54:48 -0400

    edit comments and add broswer quit function.

--- reviews.py
+++ reviews.py
@@ -1,4 +1,7 @@
-# This project displays customer reviews for James Kraus while employed at Scheller's Fitness and Cycling.
+# This module displays customer reviews for James Kraus while employed at Scheller's Fitness and Cycling.
+
+# pip install selenium
+# brew install chromedriver
 
 # Import required packages
 import time
@@ -13,7 +16,7 @@ chromeOptions.add_argument("--headless")
 chromeOptions.add_argument("--disable-gpu")
 
 
-# Locate elements by name
+# Open chrome browser, navigate to listen360 URL, and locate the HTML body element.
 browser = webdriver.Chrome(options=chromeOptions)
 browser.get("https://reviews.listen360.com/scheller-s-fitness-cycling-middletown-louisville")
 time.sleep(1)
@@ -21,11 +24,11 @@ time.sleep(1)
 element = browser.find_element_by_tag_name("body")
 
 # Scroll down on page to load older reviews on the infinite feed.
-no_of_pagedowns = 10
+no_of_pagedowns = 100
 
 while no_of_pagedowns:
     element.send_keys(Keys.PAGE_DOWN)
-    time.sleep(0.2)
+    time.sleep(0.05)
     no_of_pagedowns-=1
 
 # Find and print only the reviews that contain the name "James"
@@ -33,3 +36,5 @@ review_elements = browser.find_elements_by_xpath("//*[contains(text(), 'James')]
 
 for review in review_elements:
     print(review.text)
+
+browser.quit()
