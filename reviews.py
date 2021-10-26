33fa8ae5b915b4a4371e3b592696a20bd04d831e
James Kraus <jameskraus@Jamess-Air.attlocal.net>
2021-10-26 18:31:34 -0400

    add option for other users to enter api keys

--- reviews.py
+++ reviews.py
@@ -1,12 +1,9 @@
 # This module displays customer reviews for James Kraus while employed at Scheller's Fitness and Cycling
 # and performs sentiment analysis on the reviews.
 
-# pip install boto3
-# pip install awscli
-    # aws configure
-    # configure aws access keys
-# pip install selenium
-# brew install chromedriver
+# Install boto3 for sentiment analysis -- pip install boto3
+# Install selenium for webscraping -- pip install selenium
+# Install chromedriver in system PATH for automated browser control: https://chromedriver.chromium.org/downloads
 
 # Import required packages
 import time
@@ -16,7 +13,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-# Add options on chrome. These options allow selenium to do its work without opening the 
+# Add options on chromedriver. These options allow selenium to do its work without opening the 
 # browser window, thus speeding up the module.
 chromeOptions = Options()
 chromeOptions.add_argument("--headless")
@@ -30,22 +27,32 @@ time.sleep(1)
 
 element = browser.find_element_by_tag_name("body")
 
-# Scroll down on page to load older reviews on the infinite feed.
-no_of_pagedowns = 10
+# Scroll down on page to load older reviews on the infinite feed. 
+# Note: increase no_of_pagedowns to load more reviews
+no_of_pagedowns = 20
 
 while no_of_pagedowns:
     element.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.05)
     no_of_pagedowns-=1
 
-# Locate reviews that contain the name "James", save as string in json, print review text
+# Locate reviews that contain the name "James", save as string in json, 
+# append json strings to list, print list of reviews.
 reviews=[]
 review_elements = browser.find_elements_by_xpath("//*[contains(text(), 'James')]")
 for review in review_elements:
     json_string = json.dumps(review.text)
     reviews.append(json_string)
+print(*reviews, sep='\n')
 
-# Sentiment analysis of review text    
+# Sentiment analysis of each review.
+# Insert AWS API keys accordingly.
+client = boto3.client(
+    's3',
+    aws_access_key_id='ACCESS_KEY',
+    aws_secret_access_key='SECRET_ACCESS_KEY'
+)
+
 comprehend = boto3.client(service_name='comprehend', region_name='us-east-2')
 
 print('Calling DetectSentiment')
