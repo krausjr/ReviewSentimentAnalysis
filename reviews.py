f058f014b0dd4656a102f35f5c174fb54e0c4baa
James Kraus <jameskraus@Jamess-Air.attlocal.net>
2021-11-08 21:07:57 -0500

    functional merge of review text and sentiment analysis dataframes

--- reviews.py
+++ reviews.py
@@ -31,8 +31,8 @@ element = browser.find_element_by_tag_name("body")
 # Scroll down on page to load older reviews on the infinite feed. 
 # Note: increase no_of_pagedowns to load more reviews
 def load_reviews():
-    # no_of_pagedowns = float(input("Input number between 1-100. Higher numbers load more reviews.\n"))
-    no_of_pagedowns = 10
+    no_of_pagedowns = float(input("Input number between 1-100. Higher numbers load more reviews.\n"))
+    # no_of_pagedowns = 
 
     while no_of_pagedowns:
         element.send_keys(Keys.PAGE_DOWN)
@@ -45,36 +45,32 @@ def find_reviews():
     review_elements = browser.find_elements_by_xpath("//*[contains(text(), 'James')]")
     for review in review_elements:
         json_string = json.dumps(review.text)
+        reviews_df.loc[len(reviews_df.index)] = [json_string]
         reviews.append(json_string)
-    # print(*reviews, sep='\n')
 
 reviews=[]
+reviews_df= pd.DataFrame(columns=['review_text'])
 
 # Sentiment analysis of each review.
 # Insert AWS API keys accordingly.
 def detect_sentiment():
-    # access_key = input("INPUT ACCESS KEY\n")
-    # secret_access_key= input("INPUT SECRET ACCESS KEY\n")
+    access_key = input("INPUT ACCESS KEY\n")
+    secret_access_key= input("INPUT SECRET ACCESS KEY\n")
 
     comprehend = boto3.client(
         'comprehend', 'us-east-2',
-        aws_access_key_id = '', #access_key
-        aws_secret_access_key = '' #secret_access_key
-    )
+        aws_access_key_id = access_key,
+        aws_secret_access_key = secret_access_key) 
 
-    with open('review_analysis.json', 'w', encoding='utf-8') as f:
-        json.dump(comprehend.batch_detect_sentiment(TextList=reviews, LanguageCode='en'), f, ensure_ascii=False, indent=4)
+    sentiment_json = comprehend.batch_detect_sentiment(TextList=reviews, LanguageCode='en')
+
+    json.dumps(sentiment_json)
+    df1=pd.json_normalize(sentiment_json, record_path=['ResultList'])
+
+    frames = [df1, reviews_df]
+    final_df = pd.concat(frames, axis=1)
     
-    with open('review_analysis.json', 'r') as f:
-        review_analysis = json.loads(f.read())
-
-    df = pd.json_normalize(review_analysis, record_path=['ResultList'], meta=['Negative', 'Positive'], errors='ignore')
-    print(df)
-
-    # review_analysis = json.loads(comprehend.batch_detect_sentiment(TextList=reviews, LanguageCode='en'), sort_keys=True, indent=4)
-    # df = pd.json_normalize(review_analysis, record_path=['ResultList'], meta=['Negative', 'Positive'])
-    # print(df)
-    
+    print(final_df)
 
 def main():
     load_reviews()
